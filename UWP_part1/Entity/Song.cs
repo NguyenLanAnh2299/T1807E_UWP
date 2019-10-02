95582b57e9b37a03901f3af25ee686adfbcadcd8
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2019-10-02 21:23:03 +0700

    uwp

---
+++ UWP_part1/Entity/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_part1.Entity
+{
+    class ListSong
+    {
+        public string Name { get; set; }
+        public string singer { get; set; }
+    }
+}
---
+++ UWP_part1/Pages/Form.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using UWP_part1.Entity;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP_part1.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Form : Page
+    {
+        public Form()
+        {
+            this.InitializeComponent();
+        }
+        private const string ApiUrl = "https://2-dot-backup-server-003.appspot.com/_api/v2/songs/post-free";
+
+
+        public object JsonConvert { get; private set; }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var menber = new Menber
+            {
+                Name = this.Name.Text,
+                description = this.Description.Text,
+                singer = this.Singer.Text,
+                author = this.Author.Text,
+                thumbnail = this.thumbnail.Text,
+                link = this.link.Text,
+
+            };
+            var httpClient = new HttpClient();
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(menber), Encoding.UTF8, "application/json");
+
+            Task<HttpResponseMessage> httpRequestMessage = httpClient.PostAsync(ApiUrl, content);
+            String responseContent = httpRequestMessage.Result.Content.ReadAsStringAsync().Result;
+            Debug.WriteLine("Response: " + responseContent);
+
+        }
+    }
+    
+}
+
